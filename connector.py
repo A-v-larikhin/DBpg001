775386b4e3b809fabda6b9dad4d686fa904f4aa7
AVL <avl@larikhin.ru>
2021-04-01 16:41:08 +0300

    создаем таблицу в базе

--- connector.py
+++ connector.py
@@ -8,10 +8,25 @@ con = psycopg2.connect(
     host='192.168.10.61'
 )
 cur = con.cursor()
-cur.execute('''CREATE TABLE TEST1
+
+# переменная для создания таблицы 'test1'
+create_table = '''CREATE TABLE TEST1
     (ADMISSION INT PRIMARY KEY NOT NULL,
     NAME TEXT NOT NULL,
     VALUE CHAR(50),
-    VALUE2 CHAR(50))''')
+    VALUE2 CHAR(50))'''
+
+# Блок для просмотра таблиц в базе и создания таблицы, если она отсутствует в списке
+select_tables = "SELECT table_name FROM information_schema.tables WHERE table_schema='public'"
+cur.execute(select_tables)
+tables = cur.fetchall()
+button = False
+for i in tables:
+    for j in i:
+        if 'test1' in j:
+            button = True
+if button == False:
+    cur.execute(create_table)
+print(tables)
 con.commit()
 con.close()
